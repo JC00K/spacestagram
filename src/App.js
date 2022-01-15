2d2a258ecaa56505df1705a0c2100db9362019f7
Jeremy Cook <jcook61592@gmail.com>
2022-01-14 20:32:24 -0500

    initial commit, needs like/dislike and shareable links

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button, Card } from '@mui/material';
+import './App.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+function App() {
+  const [marsImages, setMarsImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getMarsImages = async () => {
+      await axios
+        .get(
+          'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=otFLnuDeQ2WuP5jw2w0PMoNVgVU2sxn6VhKqbtUe'
+        )
+        .then((res) => {
+          setMarsImages(res.data.photos);
+          setLoading(true);
+        });
+    };
+    getMarsImages();
+  }, []);
+
+  return loading ? (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='outer-container'>
+          {marsImages.map((image) => (
+            <Card
+              className='child-container'
+              style={{ backgroundColor: 'lightblue' }}
+            >
+              <h5>{image.camera.full_name}</h5>
+              <h5>{image.earth_date}</h5>
+              <h4>
+                <Button type='submit' color='primary'>
+                  Like
+                </Button>
+                <Button type='submit' color='error'>
+                  Dislike
+                </Button>
+                <br />
+                <Button type='link'>Get Shareable Link</Button>
+              </h4>
+              <img src={image.img_src} alt='' />
+            </Card>
+          ))}
+        </div>
+      </header>
+    </div>
+  ) : (
+    <div className='loading-screen'>
+      <h1 className='loading-message'>Retrieving Data</h1>
+    </div>
+  );
+}
+
+export default App;
